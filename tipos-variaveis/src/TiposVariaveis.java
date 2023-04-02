4431689ad5523baf714ad1f89a6f8d36a1d0f2f6
Pedro Ivo <pedroivobarretogomes26@gmail.com>
2023-04-02 10:57:37 -0300

    Alterando

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,18 @@
 public class TiposVariaveis {
+    /**
+     * int so pode numeros interiros numeros inteiros
+     * float so pode numeros decimais ex: 2,0 = 2.0F
+     * double pode digitar 200 saida 200.0
+     * long numeros grandes
+     */
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int  idade = 20;
+        float numeroDecimal = 20.0F;
+        double salario = 20000;
+        long  numeroGrande= 20000000000L;
+        System.out.println(idade);
+        System.out.println(salario);
+        System.out.println(numeroGrande);
+        System.out.println(numeroDecimal);
     }
 }
